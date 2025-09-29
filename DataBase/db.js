8e480684cb8d9eb38ea9f7e8d8b02462e58d5fb5
sarathchandrabajjanki <sarathchandrabajjanki@gmail.com>
2025-09-29 13:16:29 +0530

    updated

--- DataBase/db.js
+++ DataBase/db.js
@@ -3,7 +3,7 @@ var mongoose=require("mongoose")
 
 async function ConnectToDatabase(){
     try {
-        mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(process.env.MONGO_URL)
         console.log("connected to data base")
     } catch (error) {
         console.log(error)
